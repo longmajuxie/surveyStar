7bc1c85239a436d44a2f66802853ad3903b2f5e6
maxingxiang <xingxiang.ma@greedyint.cn>
2017-03-27 19:14:54 +0800

    1

--- src/app/pages/pages.component.spec.ts
+++ src/app/pages/pages.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { PagesComponent } from './pages.component';
+import { Pages } from './pages.component';
 
 describe('PagesComponent', () => {
-  let component: PagesComponent;
-  let fixture: ComponentFixture<PagesComponent>;
+  let component: Pages;
+  let fixture: ComponentFixture<Pages>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ PagesComponent ]
+      declarations: [ Pages ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(PagesComponent);
+    fixture = TestBed.createComponent(Pages);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
