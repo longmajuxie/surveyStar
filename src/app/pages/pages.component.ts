f1fd5148d61b82a15140fbbab4341773c03beebc
maxingxiang <xingxiang.ma@greedyint.cn>
2017-03-28 15:18:46 +0800

    滚动条

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit,HostListener, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/platform-browser';
 @Component({
   selector: 'pages',
   templateUrl: './pages.component.html',
   styleUrls: ['./pages.component.scss']
 })
 export class Pages implements OnInit {
-
-  constructor() { }
+public navIsFixed: boolean = false;
+  constructor(@Inject(DOCUMENT) private document: Document) {
+   }
 
   ngOnInit() {
   }
+  @HostListener("window:scroll", [])
+  onWindowScroll() {
+    //we'll do some stuff here when the window is scrolled
+    let number = this.document.body.scrollTop;
+    if (number > screen.availHeight) {
+      this.navIsFixed = true;
+    } else{
+      this.navIsFixed = false;
+    }
+}
+   public goTop(){
+    window.scrollTo(0, 0)
+   }
 
 }
