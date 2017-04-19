d76264552657da835a376d85a5dae0b330264cd8
maxingxiang <xingxiang.ma@greedyint.cn>
2017-04-19 19:04:08 +0800

    http请求

--- src/app/pages/questionnaire/questionnaire.component.ts
+++ src/app/pages/questionnaire/questionnaire.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Http, Response, Headers, RequestOptions} from "@angular/http";
 
 @Component({
   selector: 'app-questionnaire',
@@ -7,9 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuestionnaireComponent implements OnInit {
 
-  constructor() { }
+  constructor(private http:Http) { }
 
   ngOnInit() {
+     var newsData = this.http.get('http://localhost:8080/SSH1/initIndex.action').map((res:Response) => res.json());
+     newsData.subscribe(
+             data=> { 
+                  console.log(newsData);
+              })
   }
 
 }
