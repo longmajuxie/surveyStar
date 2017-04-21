01d7dea3b996617bbe178ff750fcba462e5ee7ec
maxingxiang <xingxiang.ma@greedyint.cn>
2017-04-21 18:45:06 +0800

    样式

--- src/app/pages/questionnaire/questionnaire-create/questionnaire-create.component.ts
+++ src/app/pages/questionnaire/questionnaire-create/questionnaire-create.component.ts
@@ -12,7 +12,18 @@ export class QuestionnaireCreateComponent implements OnInit {
     questionnairePrompt:"欢迎参加调查！",
     questionList:[]
   };
-  questionGenre={}
+ questionGenre={
+    0:"单选题",
+    1:"下拉选择题",
+    2:"多选题",
+    3:"单行填空题",
+    4:"多行填空题",
+    5:"矩阵单选题",
+    6:"矩阵多选题",
+    7:"图片单选",
+    8:"图片多选",
+    9:"描述说明",
+  }
   constructor() { }
 
   ngOnInit() {
@@ -22,7 +33,7 @@ export class QuestionnaireCreateComponent implements OnInit {
   public addQuestion=function(index){
        let question={
                  questionGenre:index, 
-                 questionTitle:"题目名",
+                 questionTitle:this.questionGenre[index],
                   questionChoice:[
                            {
                                 text:"选项内容",
--- src/app/pages/questionnaire/questionnaire-list/questionnaire-list.component.ts
+++ src/app/pages/questionnaire/questionnaire-list/questionnaire-list.component.ts
@@ -10,6 +10,7 @@ export class QuestionnaireListComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    
   }
 
 }
