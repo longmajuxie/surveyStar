9b34ebfc2be9140d5fea5d92d41f0cf912539601
maxingxiang <xingxiang.ma@greedyint.cn>
2017-04-24 14:21:56 +0800

    优化

--- src/app/pages/questionnaire/questionnaire-create/questionnaire-create.component.ts
+++ src/app/pages/questionnaire/questionnaire-create/questionnaire-create.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
+import { QuestionnaireDelService } from './questionnaire-create.service';
 
 @Component({
   selector: 'app-questionnaire-create',
   templateUrl: './questionnaire-create.component.html',
   styleUrls: ['./questionnaire-create.component.scss'],
-   encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.None,
 })
 export class QuestionnaireCreateComponent implements OnInit {
   
@@ -13,45 +14,15 @@ export class QuestionnaireCreateComponent implements OnInit {
     questionnairePrompt:"欢迎参加调查！",
     questionList:[]
   };
- questionGenre={
-    0:"单选题",
-    1:"下拉选择题",
-    2:"多选题",
-    3:"单行填空题",
-    4:"多行填空题",
-    5:"矩阵单选题",
-    6:"矩阵多选题",
-    7:"图片单选",
-    8:"图片多选",
-    9:"描述说明",
-  }
-  constructor() { }
+  constructor(public del:QuestionnaireDelService) { }
 
   ngOnInit() {
       
   }
   
   public addQuestion=function(index){
-       let question={
-                 questionGenre:index,
-                 questionTitle:this.questionGenre[index],
-                 isnecessary:0,     //0表示必做，1表示选做
-                 questionChoice:[
-                           {
-                                text:"选项内容",
-                                isSelected:false
-                            },
-                             {
-                                text:"选项内容",
-                                isSelected:false
-                            },
-                             {
-                                text:"选项内容",
-                                isSelected:false
-                            }
-                   ]
-           
-              };
+       let question={};
+       question=this.del.createQuestionnaireQuestion(index);
        this.qustionnaire.questionList.push(question);
        console.log(this.qustionnaire);
   }
@@ -59,6 +30,7 @@ export class QuestionnaireCreateComponent implements OnInit {
   public questionDelete=function(index){
     this.qustionnaire.questionList.splice(index,1);
   }
+  
 }
 
 /*
---
+++ src/app/pages/questionnaire/questionnaire-create/questionnaire-create.service.ts
@@ -0,0 +1,115 @@
+import {Injectable,OnInit} from '@angular/core';
+
+
+
+@Injectable()
+export class QuestionnaireDelService implements OnInit {
+    questionGenre={
+         0:"单选题",
+            1:"下拉选择题",
+            2:"多选题",
+            3:"单行填空题",
+            4:"多行填空题",
+            5:"矩阵单选题",
+            6:"矩阵多选题",
+            7:"图片单选",
+            8:"图片多选",
+            9:"描述说明",
+    };
+    constructor() {
+    }
+    ngOnInit(){
+    }
+    createQuestionnaireQuestion(index){
+            let question={
+                 questionGenre:index,
+                 questionTitle:this.questionGenre[index],
+                 isnecessary:0,     //0表示必做，1表示选做
+                 questionChoice:[]
+              };
+              let questionChoice=[];
+              if(index==0||index==1||index==2){
+                questionChoice.push(
+                            {
+                                text:"选项内容1",
+                                isSelected:false
+                            },
+                             {
+                                text:"选项内容2",
+                                isSelected:false
+                            },
+                             {
+                                text:"选项内容3",
+                                isSelected:false
+                            }
+                );
+              }
+              else if(index==3||index==4||index==9){
+                questionChoice.push( {
+                                text:"",
+                                isSelected:false
+                            },);
+              }
+              else if(index==5||index==6){
+                questionChoice.push(
+                    {
+                        line:"",
+                        choice:[
+                            {
+                                text:"选项内容1",
+                                isSelected:false
+                            },
+                            {
+                                text:"选项内容2",
+                                isSelected:false
+                            },
+                            {
+                                text:"选项内容3",
+                                isSelected:false
+                            },
+                        ]
+                    },
+                     {
+                        line:"矩阵行1",
+                        choice:[
+                            {
+                                text:"选项内容1",
+                                isSelected:false
+                            },
+                            {
+                                text:"选项内容2",
+                                isSelected:false
+                            },
+                            {
+                                text:"选项内容3",
+                                isSelected:false
+                            },
+                        ]
+                    },
+                     {
+                        line:"矩阵行2",
+                        choice:[
+                            {
+                                text:"选项内容1",
+                                isSelected:false
+                            },
+                            {
+                                text:"选项内容2",
+                                isSelected:false
+                            },
+                            {
+                                text:"选项内容3",
+                                isSelected:false
+                            },
+                        ]
+                    }
+                );
+              }
+              else if(index==7||index==8){
+                questionChoice.push();
+              }
+       question.questionChoice.push(questionChoice);
+       return question;
+    }
+   
+}
